65721218fcfc1d5dc2a5999bc2f9567495682b9f
祝霄朗 <3430935799@qq.com>
2021-02-02 16:13:37 +0800

    Create 源.cpp

---
+++ 源.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("int = %d\n", sizeof(int));
+	printf("short int = %d\n", sizeof(short int));
+	printf("long int = %d\n", sizeof(long int));
+	printf("long long int = %d\n", sizeof(long long int));
+	printf("char = %d\n", sizeof(char));
+	printf("bool = %d\n", sizeof(bool));
+	printf("float = %d\n", sizeof(float));
+	printf("double = %d\n", sizeof(double));
+
+    return 0;
+
+}
